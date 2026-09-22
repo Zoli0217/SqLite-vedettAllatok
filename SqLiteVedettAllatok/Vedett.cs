ac84a5da3824cb3d55227c3def3e246bec243a74
Zoli0217 <zoltanbodi61@gmail.com>
2026-09-22 14:13:49 +0200

    first commit

---
+++ SqLiteVedettAllatok/Vedett.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SqLiteVedettAllatok
+{
+    internal class Vedett
+    {
+        public Vedett(int id, string nev, int ertek, int ev, string osztaly)
+        {
+            Id = id;
+            Nev = nev;
+            Ertek = ertek;
+            Ev = ev;
+            Osztaly = osztaly;
+        }
+
+        public int Id { get; set; }
+        public string Nev { get; set; }
+        public int Ertek { get; set; }
+        public int Ev { get; set; }
+        public string Osztaly { get; set; }
+    }
+}
