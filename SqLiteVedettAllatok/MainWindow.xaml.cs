33b5a33fd9019e4f13433776d4b300f95e71eb42
Zoli0217 <zoltanbodi61@gmail.com>
2026-09-23 11:28:28 +0200

    kesz

--- SqLiteVedettAllatok/MainWindow.xaml.cs
+++ SqLiteVedettAllatok/MainWindow.xaml.cs
@@ -28,14 +28,17 @@ namespace SqLiteVedettAllatok
             connection = new SqliteConnection(ConnectionString);
             connection.Open();
 
+            ReadFromDatabase();
+            ReadBesoroloas();
+
         }
 
         public void ReadFromDatabase()
         {
             string queryText = """
-                SELECT nev, ertek, ev, besorolas.id, osztaly
-                FROM vedett
-                INNER JOIN besorolas ON vedett.besorolas_id = besorolas.id
+                SELECT vedett_allat.id, vedett_allat.nev, ertek, ev, besorolas.nev AS osztaly
+                FROM vedett_allat
+                INNER JOIN besorolas ON vedett_allat.besorolas_id = besorolas.id
                 """;
 
             SqliteCommand command = new SqliteCommand(queryText, connection);
@@ -45,16 +48,74 @@ namespace SqLiteVedettAllatok
 
             while (reader.Read())
             {
-                string nev = reader.GetString(0);
-                int ertek = reader.GetInt32(1);
-                int ev = reader.GetInt32(2);
-                int id = reader.GetInt32(3);
+                int id = reader.GetInt32(0);
+                string nev = reader.GetString(1);
+                int ertek = reader.GetInt32(2);
+                int ev = reader.GetInt32(3);
                 string osztaly = reader.GetString(4);
                 dataList.Add(new Vedett(id, nev, ertek, ev, osztaly));
             }
             reader.Close();
 
             dataGrid.ItemsSource = dataList;
+        }
+
+        public void ReadBesoroloas()
+        {
+            string queryText = "Select nev from besorolas";
+            SqliteCommand command = new SqliteCommand(queryText, connection);
+            SqliteDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                string nev = reader.GetString(0);
+                osztalyCbx.Items.Add(nev);
+            }
+            reader.Close();
+        }
+
+        private void deleteBtn(object sender, RoutedEventArgs e)
+        {
+            List<Vedett> selectedItems = dataGrid.SelectedItems.Cast<Vedett>().ToList();
+            if(selectedItems != null)
+            {
+                foreach (var item in selectedItems)
+                {
+                    dataList.Remove(item);
+                    string deleteText = "DELETE FROM vedett_allat WHERE id = @id";
+                    SqliteCommand deleteCommand = new SqliteCommand(deleteText, connection);
+                    deleteCommand.Parameters.AddWithValue("@id", item.Id);
+
+                    deleteCommand.ExecuteNonQuery();
+                }
+                dataGrid.Items.Refresh();
+            }
+        }
+
+        private void osztalyCbx_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var filteredList = dataList.Where(x => x.Osztaly == osztalyCbx.SelectedItem.ToString());
+            dataGrid.ItemsSource = filteredList.ToList();
+
+
+            avgPriceLb.Content = $"Átlagos érték: {filteredList.Average(x => x.Ertek):F0}Ft";
+        }
+
+        private void resetBtn(object sender, RoutedEventArgs e)
+        {
+            dataGrid.ItemsSource = dataList;
+        }
+
+        private void updateBtn(object sender, RoutedEventArgs e)
+        {
+
+            var lista = dataGrid.Items.Cast<Vedett>().ToList();
+
+            var max = lista.Max(x => x.Ertek);
+            var maxPriceList = lista.Where(x => x.Ertek == max).ToList();
+            maxPriceLb.Content = "Legdrágább állat(ok):";
+            maxPriceList.ForEach(x => maxPriceLb.Content += $"\n{x.Nev}");
+
         }
     }
 }
